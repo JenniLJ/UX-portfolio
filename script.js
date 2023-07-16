1eb4151eea3b9aff9680486a8657ab838b95000e
JenniLJ <48532943+JenniLJ@users.noreply.github.com>
2023-07-16 00:34:50 -0500

    toggling menu to expand and odllapse wip

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //this is a javascript file. There will be javascript all over this thing
+// Add JavaScript to handle the menu toggle behavior
+const menuToggle = document.getElementById("menu-toggle");
+const closeBtn = document.querySelector(".close-btn");
+
+menuToggle.addEventListener("change", function () {
+  if (this.checked) {
+    closeBtn.style.display = "block"; // Show the close button when menu is expanded
+  } else {
+    closeBtn.style.display = "none"; // Hide the close button when menu is collapsed
+  }
+});
